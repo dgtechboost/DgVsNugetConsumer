ed20cc6b0fbfa16a20f3272c379b6d47245cced2
dgtechboost <25640061+dgtechboost@users.noreply.github.com>
2021-06-03 14:34:56 +0100

    add package, use package method, update package

--- DgVsNugetConsumer/Controllers/WeatherForecastController.cs
+++ DgVsNugetConsumer/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DgLoggers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,15 +18,26 @@ namespace DgVsNugetConsumer.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ITextLogger _textLogger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            ITextLogger textLogger
+            )
         {
             _logger = logger;
+            _textLogger = textLogger;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            //here we initiate the package logger
+            var pckgLogger = new TextLogger();
+            pckgLogger.Log("test logger");
+
+            _textLogger.Log("test logger update");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- DgVsNugetConsumer/Startup.cs
+++ DgVsNugetConsumer/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DgLoggers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace DgVsNugetConsumer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ITextLogger, TextLogger>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
